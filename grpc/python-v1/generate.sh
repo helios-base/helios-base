f960c46e2da1e2a6a97f6032b2c1507cdae1919b
naderzare <nader.zare88@gmail.com>
2024-01-06 15:21:25 -0400

    remove grpc python

--- grpc/python-v1/generate.sh
+++
@@ -1,1 +0,0 @@
-python3 -m grpc_tools.protoc -I../protos --python_out=. --pyi_out=. --grpc_python_out=. ../protos/*.proto
--- grpc/python-v1/src/IDecisionMaker.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-import service_pb2 as pb2
-from src.IAgent import IAgent
-
-
-class IDecisionMaker(ABC):
-    @abstractmethod
-    def make_decision(self, agent: IAgent):
-        pass
